395579b7bca7f62a78a5bd99ee073d2e96d7d6ae
John MacTavish <john@techsolx.com>
2020-09-15 02:23:30 -0700

    Modify .gitignore,
    stub out tests
    work on app.py.
    
    TODO: Docstrings, tests

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import apistar
+import datetime
+import ipaddress
 import logging
 import os
+import socket
 import sqlite3
-import sys
 import urllib.request
 
 """
@@ -16,17 +18,27 @@ It will then connect to Route53 and update the DNS record as necessary.
 Might have it do other stuff too as I think about it...
 """
 
-logging.basicConfig(stream=sys.stderr, level=logging.CRITICAL)
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sites_path = r"../sitestocheck/"
 db_path = r"../db/"
 db_file = "addresschanges.db"
+database = os.path.join(dir_path, db_path, db_file)
+
 
 # From https://www.sqlitetutorial.net/sqlite-python/create-tables/
 
 
 def create_connection(db_file):
-    """ create a database connection to the SQLite database
+    """create a database connection to the SQLite database
     specified by db_file
     :param db_file: database file
     :return: Connection object or None
@@ -42,11 +54,11 @@ def create_connection(db_file):
 
 
 def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
+    """create a table from the create_table_sql statement
     :param conn: Connection object
     :param create_table_sql: a CREATE TABLE statement
     :return:
-        """
+    """
     try:
         c = conn.cursor()
         c.execute(create_table_sql)
@@ -54,8 +66,7 @@ def create_table(conn, create_table_sql):
         print(e)
 
 
-def create_db():
-    database = os.path.join(db_path, db_file)
+def create_db(database):
 
     sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS ipchanges (
                                     id integer PRIMARY KEY,
@@ -64,7 +75,7 @@ def create_db():
                                     site_checked text
                                     ); """
 
-# create a database connection
+    # create a database connection
     conn = create_connection(database)
 
     # create tables
@@ -76,9 +87,59 @@ def create_db():
         print("Error! cannot create the database connection.")
 
 
+def create_entry(conn, entry):
+    """
+    Create a new entry into the ipchanges table
+    :param conn:
+    :param entry:
+    :return: ipchanges id
+    """
+    sql = ''' INSERT INTO ipchanges(ip_address,change_date,site_checked)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, entry)
+    conn.commit()
+    return cur.lastrowid
+
+
+def is_connected():
+    try:
+        # connect to the host -- tells us if the internet connection is live
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        pass
+    return False
+
+
+def get_ip(site):
+    try:
+        req = urllib.request.Request(site)
+        response = urllib.request.urlopen(req)
+        target_ip = response.read().decode("utf-8")
+        try:
+            ipaddress.IPv4Address(target_ip)
+            return target_ip
+        except ipaddress.AddressValueError as e:
+            return f"address_error {e}"
+
+    except urllib.error.URLError as e:
+        return f"URLError {e}"
+
+
 def main():
-    create_db
+    create_db(database)
+
+    if is_connected():
+        found_ip = get_ip("https://api.ipify.org/")
+        conn = create_connection(database)
+
+    with conn:
+        timestamp = datetime.datetime.now()
+        entry = (found_ip, timestamp, "ipify.org")
+        create_entry(conn, entry)
+        logger.INFO(f"entry is {entry}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
